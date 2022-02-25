5d3e94f5bb1326d192a73dfe3d2711e743575cf2
tsedveet <tsedvee.tt@gmail.com>
2022-02-25 20:33:35 +0800

    change font again

--- src/components/top-side/index.jsx
+++ src/components/top-side/index.jsx
@@ -2,6 +2,6 @@ import React from "react";
 
 export const Top = props => (
     <div className="bg-black">
-        <p className="text-center font-architects text-white py-2 text-sm">{props.top_text}</p>
+        <p className="text-center text-white py-2 text-sm">{props.top_text}</p>
     </div>
 );

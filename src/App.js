61a4776d8f1f220c3416cdf2361fb95531d60093
tsedveet <tsedvee.tt@gmail.com>
2022-02-25 20:29:24 +0800

    font-zassan

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ export default class App extends Component {
       <div className="">
         <Top top_text="Муурнуудаас хайлт хийдэг жижигхэн ReactJS-app 👌😒😵‍💫" />
         <div className="max-w-6xl mx-auto">
-          <h1 className="text-3xl md:text-5xl uppercase font-bold py-4 font-oswald border rounded text-center bg-gray-300">
+          <h1 className="text-3xl md:text-4xl uppercase font-bold py-4 font-oswald border rounded text-center bg-gray-300">
             Муурнуудын хайлт
           </h1>
           <CardList robots={this.state.robots} />
